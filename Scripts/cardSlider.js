77a3f3b0b664032ec4578ed7cdcad2ef8961f09b
brendagiron <brendagironbarbosa2007@gmail.com>
2024-07-07 22:18:36 -0300

    Novo carousel responsivo para planos

--- Scripts/cardSlider.js
+++ Scripts/cardSlider.js
@@ -1,17 +1,36 @@
-const slider = document.querySelector('.slider-container');
-const leftButton = document.querySelector('.slider-left-button');
-const rightButton = document.querySelector('.slider-right-button');
-
-rightButton.addEventListener('click', () => {
-  slider.scrollBy({
-    left: 350, // Largura do card + margem direita
-    behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', function() {
+    const leftButton = document.querySelector('.slider-left-button');
+    const rightButton = document.querySelector('.slider-right-button');
+    const carousel = document.querySelector('.itens-carrosel');
+  
+    // Calcula a largura de um item no carrossel
+    let itemWidth = carousel.querySelector('.item').offsetWidth;
+  
+    // Inicializa o índice do item atual
+    let currentIndex = 0;
+  
+    // Adiciona evento de clique no botão "Anterior"
+    leftButton.addEventListener('click', function() {
+      if (currentIndex > 0) {
+        currentIndex--;
+      }
+      updateCarousel();
+    });
+  
+    // Adiciona evento de clique no botão "Próximo"
+    rightButton.addEventListener('click', function() {
+      if (currentIndex < carousel.children.length - 1) {
+        currentIndex++;
+      }
+      updateCarousel();
+    });
+  
+    // Função para atualizar a posição do carrossel com base no índice atual
+    function updateCarousel() {
+      itemWidth = carousel.querySelector('.item').offsetWidth;
+      const offset = -currentIndex * itemWidth;
+      carousel.style.transform = `translateX(${offset}px)`;
+    }
   });
-});
-
-leftButton.addEventListener('click', () => {
-  slider.scrollBy({
-    left: -350,
-    behavior: 'smooth'
-  });
-});
+  
+  
